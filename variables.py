f80c2aac0cce0a22600b81c41d0520049b4bdb1f
Ray Lyon <ray@raylyon.net>
2020-01-16 18:38:13 +0000

    variables exercise

---
+++ variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.8
+
+first_name = 'Ray'
+last_name = 'Lyon'
+age = 29
+birth_date = 'December 26th, 1990'
+
+
+print("My name is", first_name, last_name+".")
+print("I was born on", birth_date, "and I'm", age,"years old.")
+
